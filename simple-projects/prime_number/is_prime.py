1fdfe8cd4eadb5732b64a25889979ca25e4adf90
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-29 17:30:23 +0300

    new commit

---
+++ simple-projects/prime_number/is_prime.py
@@ -0,0 +1,19 @@
+user_input = int(input("Please enter an integer: "))
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    for i in range(2, int(number / 2) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+if is_prime(user_input):
+    print(f"'{user_input} is a prime number.")
+else:
+    print(f"'{user_input} is not a prime number.")
+
+
