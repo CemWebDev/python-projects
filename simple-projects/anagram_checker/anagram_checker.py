12a03d04ad4e08ae6f7a1113ed3b79d6e17a4c3a
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-29 16:02:40 +0300

    end

--- simple-projects/anagram_checker/anagram_checker.py
+++ simple-projects/anagram_checker/anagram_checker.py
@@ -4,7 +4,7 @@ def is_anagram(first_word, second_word):
         for char in string_value:
             if char.isalpha():
                 result += char
-            return result.lower()
+        return result.lower()
     def get_char_freq(input_word):
         frequency = {}
         for char in input_word:
@@ -12,11 +12,21 @@ def is_anagram(first_word, second_word):
                 frequency[char] += 1
             else:
                 frequency[char] = 1
-            return frequency
+        return frequency
         
+    cleaned_word_first = remove_non_alpha(first_word)
+    cleaned_word_second = remove_non_alpha(second_word)
 
-
+    first_frequency = get_char_freq(cleaned_word_first)
+    second_frequency = get_char_freq(cleaned_word_second)
+    
+    return first_frequency == second_frequency
 
 
 first_string = input("PLease enter the first word: ")
 second_string = input("Please enter the second string: ")
+
+if is_anagram(first_string, second_string):
+    print(f"'{first_string}' and '{second_string}' are anagrams.")
+else:
+    print(f"'{first_string}' and '{second_string}' are not anagrams.")
