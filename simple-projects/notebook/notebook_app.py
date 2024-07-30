0153d602fd56e4b1d7a62ec326ddac6f5d306bf5
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-30 20:12:21 +0300

    format the file

--- simple-projects/notebook/notebook_app.py
+++ simple-projects/notebook/notebook_app.py
@@ -19,11 +19,12 @@ def view_notes(notes_list):
     else:
         print("There no any notes, let's add some!")
 
+
 def delete_notes(notes_list):
     if notes_list:
         try:
             view_notes(notes_list)
-            deleting_index = int(input("Which note dou you want to delete: ")) - 1    
+            deleting_index = int(input("Which note dou you want to delete: ")) - 1
             if 0 <= deleting_index < len(notes_list):
                 while True:
                     confirm = input("Are you sure you want to delete this note? (yes/no) ").lower()
@@ -31,12 +32,12 @@ def delete_notes(notes_list):
                         removed_note = notes_list.pop(deleting_index)
                         print(f"'{removed_note}' deleted.")
                         break
-                    else: 
+                    else:
                         print("Deletion canceled!")
                         break
-                    
+
         except ValueError:
-            print("Invalid note number!")    
+            print("Invalid note number!")
     else:
         print("There no any notes, let's add some!")
 
@@ -56,7 +57,7 @@ def main():
             print("Exit...")
             break
         else:
-            print("Invalid operation!") 
+            print("Invalid operation!")
 
 
 main()
