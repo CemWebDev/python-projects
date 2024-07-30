b1ee586f87d2e7b4c98ddd644e77d1e5ade08bfb
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-30 20:13:37 +0300

    format the file

--- simple-projects/rock-paper-scissors/rock_paper_scissors.py
+++ simple-projects/rock-paper-scissors/rock_paper_scissors.py
@@ -2,8 +2,10 @@ import random
 
 items = ["rock", "paper", "scissors"]
 
+
 def get_computer_choice():
     return random.choice(items)
+
 
 def get_user_choice():
     while True:
@@ -12,24 +14,29 @@ def get_user_choice():
             return user_input
         print("Invalid")
 
+
 def winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Same item")
-    elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "scissors" and computer_choice == "paper") or (user_choice == "paper" and computer_choice == "rock"):
+    elif (user_choice == "rock" and computer_choice == "scissors") or (
+            user_choice == "scissors" and computer_choice == "paper") or (
+            user_choice == "paper" and computer_choice == "rock"):
         print("You win against to computer!")
     else:
         print("Computer Wins!")
     print(f"Your choice: {user_choice}")
     print(f"Computers choice: {computer_choice}")
-        
+
+
 def start_game():
     while True:
         user_choice = get_user_choice()
         computer_choice = get_computer_choice()
         winner(user_choice, computer_choice)
-        
+
         query = input("Do you want to play again? (yes/no): ").lower()
         if query != "yes":
             break
-    
+
+
 start_game()
