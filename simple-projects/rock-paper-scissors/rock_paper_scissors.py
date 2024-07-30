7f2e4598b4bf0cf3193da5f2772998b7de82e193
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-30 06:59:50 +0300

    new commit

---
+++ simple-projects/rock-paper-scissors/rock_paper_scissors.py
@@ -0,0 +1,17 @@
+import random
+
+items = ["rock", "paper", "scissors"]
+
+def get_computer_choice():
+    return random.choice(items)
+
+def get_user_choice():
+    while True:
+        user_input = input("Rock, paper or scissors? ").lower()
+        if user_input in items:
+            return user_input
+        print("Invalid")
+
+def winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("Same item1")
