404c2982477f60a8e83e693e49faa7862fd380ae
SimplyProgYT <bcemcankaya@gmail.com>
2024-07-30 19:39:23 +0300

    new commit

--- simple-projects/notebook/notebook_app.py
+++ simple-projects/notebook/notebook_app.py
@@ -4,18 +4,40 @@ def display_menu():
     print("2-See notes")
     print("3-Delete notes")
     print("4-Exit")
-    
-    
-    
+
+
 def add_notes(notes_list):
     new_note = input("Write you note here: ")
     notes_list.append(new_note)
     print("New note added successfully!")
-    
+
+
 def view_notes(notes_list):
     if notes_list:
         for index, note in enumerate(notes_list, start=1):
             print(f"{index}-{note}")
+    else:
+        print("There no any notes, let's add some!")
+
+def delete_notes(notes_list):
+    if notes_list:
+        try:
+            print("Which one do you want to delete: ")
+            view_notes()
+            deleting_index = int(input("1, 2 ...")) - 1    
+            if 0 <= deleting_index < len(notes_list):
+                while True:
+                    confirm = input("Are you sure you want to delete this note? (yes/no) ").lower()
+                    if confirm != 'no':
+                        removed_note = notes_list.pop(deleting_index)
+                        print(f"{removed_note} deleted.")
+                        break
+                    else: 
+                        print("Deletion canceled!")
+                        break
+                    
+        except ValueError:
+            print("Invalid note number!")    
     else:
         print("There no any notes, let's add some!")
 
@@ -30,7 +52,7 @@ def main():
         elif user_choice == "2":
             print()
         elif user_choice == "3":
-            print()
+            delete_notes(notes)
         elif user_choice == "4":
             print("Exit...")
             break
