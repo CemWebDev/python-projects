604349101608490bcfbb6289c74902b4320c4385
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-04 12:01:21 +0300

    creating generate_color_code func

--- simple-projects/color_match/color_game.py
+++ simple-projects/color_match/color_game.py
@@ -1,2 +1,14 @@
 import random
 
+colors = ['R', 'G', 'B', 'P', 'O', 'W']
+attempts = 10
+color_code_length = 4
+
+def generate_color_code():
+    code = []
+    for color in range(color_code_length):
+        color = random.choice(colors)
+        code.append(color)
+        
+    return code
+        
