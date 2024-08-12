a8ebbe82849a1d1e04a7a5cb588b650d82cd923f
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-13 00:38:21 +0300

    creating the matrix_utils module

--- linear-algebra/determinant-computation/determinant.py
+++ linear-algebra/determinant-computation/determinant.py
@@ -1,20 +1,7 @@
 import numpy as np
+from matrix_utils import get_square_matrix
 
-def get_matrix():
-    rows = int(input('Enter the number of rows: '))
-    cols = rows
-        
-    print(f'Enter the elements of the matrix {rows}x{cols}')
-    matrix = []
-    for i in range(rows):
-        row = list(map(float, input(f'Row {i+1} (separate element with space): ').split()))
-        if len(row) != cols:
-            raise ValueError('Number of elements does not match the number of columns!')
-        matrix.append(row)
-        
-    return np.array(matrix)
-
-matrix = get_matrix()
+matrix = get_square_matrix()
 
 try:
     determinant = np.linalg.det(matrix)
---
+++ linear-algebra/matrix_utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def get_square_matrix():
+    rows = int(input('Enter the number of rows'))
+    cols = rows
+    
+    matrix = []
+    for i in range(rows):
+        row = list(map(float, input(f'Row {i+1} (separate elements with space): ').split()))
+        if len(row) != cols:
+            raise ValueError('Number of elements does not match the number of columns!')
+        matrix.append(row)
+        
+    return np.array(matrix)
