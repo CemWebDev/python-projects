39a6de5fa11a4893e99f41f1bc75c7ecea29de51
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-13 00:04:29 +0300

    finished project

--- linear-algebra/determinant-computation/determinant.py
+++ linear-algebra/determinant-computation/determinant.py
@@ -10,7 +10,7 @@ def get_matrix():
     print(f'Enter the elements of the matrix {rows}x{cols}')
     matrix = []
     for i in range(rows):
-        row = list(map(float, input(f'Row {i+1} (separate element with space): '))).split()
+        row = list(map(float, input(f'Row {i+1} (separate element with space): ').split()))
         if len(row) != cols:
             raise ValueError('Number of elements does not match the number of columns!')
         matrix.append(row)
@@ -18,3 +18,10 @@ def get_matrix():
     return np.array(matrix)
 
 matrix = get_matrix()
+
+try:
+    determinant = np.linalg.det(matrix)
+    print(f'Matrix: {matrix}')
+    print(f'Determinant of the matrix: {determinant}')
+except ValueError as e:
+    print(f'An error occurred {e}')
