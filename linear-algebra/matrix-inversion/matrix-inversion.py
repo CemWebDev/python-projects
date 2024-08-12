9a4b6e24772f58a45509772848d8ce7896557157
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-13 00:48:29 +0300

    finished matrix-inversion

--- linear-algebra/matrix-inversion/matrix-inversion.py
+++ linear-algebra/matrix-inversion/matrix-inversion.py
@@ -9,3 +9,10 @@ def compute_inverse(matrix):
         raise ValueError('The matrix is not invertible because its determinant is zero.')
 
 matrix = get_square_matrix()
+
+try:
+    inverse_matrix = compute_inverse(matrix)
+    print(f'Matrix: {matrix}')
+    print(f'Inverse matrix: {inverse_matrix}')
+except ValueError as e:
+    print(f'An error occurred {e}')
