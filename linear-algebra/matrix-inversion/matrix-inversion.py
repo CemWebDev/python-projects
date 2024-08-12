f0f040827ebcce144d7ef0f59272aa2f4246db1a
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-13 00:46:41 +0300

    creating compute_matrix func

--- linear-algebra/matrix-inversion/matrix-inversion.py
+++ linear-algebra/matrix-inversion/matrix-inversion.py
@@ -1,2 +1,11 @@
 import numpy as np
+from matrix_utils import get_square_matrix
 
+def compute_inverse(matrix):
+    try:
+        inverse_matrix = np.linalg.inv(matrix)
+        return inverse_matrix
+    except np.linalg.LinAlgError:
+        raise ValueError('The matrix is not invertible because its determinant is zero.')
+
+matrix = get_square_matrix()
