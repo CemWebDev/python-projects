b67bb5743667074b9e5608beaca7a0ae80a5bdea
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-13 15:13:08 +0300

    updated determinant.py

--- linear-algebra/determinant.py
+++ linear-algebra/determinant.py
@@ -3,9 +3,13 @@ from matrix_utils import determine_matrix
 
 matrix = determine_matrix(square_matrix=True)
 
-try:
-    determinant = np.linalg.det(matrix)
-    print(f'Matrix: {matrix}')
-    print(f'Determinant of the matrix: {determinant}')
-except ValueError as e:
-    print(f'An error occurred {e}')
+def compute_determinant():
+    try:
+        determinant = np.linalg.det(matrix)
+    except ValueError as e:
+        print(f'An error occurred {e}')
+        determinant = None
+    
+    return determinant
+
+print(f'Determinant: {compute_determinant()}')
