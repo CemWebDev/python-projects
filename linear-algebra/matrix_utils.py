6ef3c5f37631b3a3f32d51f8d5e119ca4e4f31a4
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-13 14:47:28 +0300

    finished matrix utils

--- linear-algebra/matrix_utils.py
+++ linear-algebra/matrix_utils.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-
-
-def determine_matrix():
-    rows = int(input('Enter the number of rows: '))
-    cols = int(input('Enter the number of cols: '))
+def determine_matrix(rows=None, cols=None, square_matrix=False):
+    if rows is None:
+        rows = int(input('Enter the number of rows: '))
+    if cols is None:
+        cols = int(input('Enter the number of columns: '))
+    if square_matrix and rows != cols:
+        raise ValueError('An error occurred! The matrix must be square.')
+    
     matrix = []
     for i in range(rows):
-        row = list(map(float, input(f'Row {i+1} (separate elements with space: ): ').split()))
+        row = list(map(float, input(f'Row {i+1} (separate elements with space): ').split()))
         if len(row) != cols:
-            raise ValueError(f'An error occurred! Row{i+1} should have {cols} elements')
-        
+            raise ValueError(f'An error occurred! Row {i+1} should have {cols} elements.')
         matrix.append(row)
-        
-    matrix = np.array(matrix)
-
-
+    
+    return np.array(matrix)
