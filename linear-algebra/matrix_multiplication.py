3aeb562bd91a39db64e491ca4ad2e469537c1ae7
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-13 13:53:52 +0300

    matrix multiplication

---
+++ linear-algebra/matrix_multiplication.py
@@ -0,0 +1,11 @@
+import numpy as np
+from matrix_utils import non_square_matrix
+
+first_matrix = non_square_matrix()
+second_matrix = non_square_matrix()
+
+if first_matrix.shape[1] != second_matrix.shape[0]:
+    print('The number of columns in thr first matrix must be equal to the number of rows in the second matrix')
+else:
+    product = np.dot(first_matrix, second_matrix)
+    print(f'[first matrix] x [second matrix]: {product}')
