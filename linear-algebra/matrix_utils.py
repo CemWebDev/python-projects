bd4cba27a88fc689ecf4ea40663eb0c38211af1a
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-13 14:31:07 +0300

    update matrix utils

--- linear-algebra/matrix_utils.py
+++ linear-algebra/matrix_utils.py
@@ -1,28 +1,18 @@
 import numpy as np
 
 
-def get_square_matrix():
-    rows = int(input('Enter the number of rows: '))
-    cols = rows
-    
-    matrix = []
-    for i in range(rows):
-        row = list(map(float, input(f'Row {i+1} (separate elements with space): ').split()))
-        if len(row) != cols:
-            raise ValueError('Number of elements does not match the number of columns!')
-        matrix.append(row)
-        
-    return np.array(matrix)
 
-
-def non_square_matrix():
+def determine_matrix():
     rows = int(input('Enter the number of rows: '))
     cols = int(input('Enter the number of cols: '))
-    
     matrix = []
     for i in range(rows):
-        row = list(map(float, input(f'Row {i+1}: ').split()))
+        row = list(map(float, input(f'Row {i+1} (separate elements with space: ): ').split()))
+        if len(row) != cols:
+            raise ValueError(f'An error occurred! Row{i+1} should have {cols} elements')
+        
         matrix.append(row)
         
-    return np.array(matrix)
+    matrix = np.array(matrix)
+
 
