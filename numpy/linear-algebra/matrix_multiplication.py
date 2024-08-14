63d81c0f0a37233d19261d378640e5db67001da7
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-14 17:17:40 +0300

    updated matrix_multiplication

--- numpy/linear-algebra/matrix_multiplication.py
+++ numpy/linear-algebra/matrix_multiplication.py
@@ -1,13 +1,13 @@
 import numpy as np
-from matrix_utils import determine_matrix
+from matrix_utils import determine_matrix as matrix
 
 def matrix_multiplication():
     print('Enter the first matrix: ')
-    first_matrix = determine_matrix()
+    first_matrix = matrix()
     
     print('Enter the second matrix: ')
     row_length = len(first_matrix[0])
-    second_matrix = determine_matrix(rows=row_length)
+    second_matrix = matrix(rows=row_length)
     
     product = np.dot(first_matrix, second_matrix)
     print(f'(First matrix) x (second matrix): {product}')
